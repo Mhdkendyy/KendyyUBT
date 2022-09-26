6ba957dc3efa6ed40445b41d46e7165508a4744f
Mhdkendyy <114293906+Mhdkendyy@users.noreply.github.com>
2022-09-26 18:44:22 +0700

    .

--- Bion/helpers/cache/__init__.py
+++ Bion/helpers/cache/__init__.py
@@ -3,5 +3,5 @@ from os import getenv
 
 CBHDSYS = getenv(
     "CBHDSYS",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL0NpbGlrUHJvamVjdC9CaW9uLVB5cm9Cb3Q=").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL0NpbGlrUHJvamVjdC9LZW5keXlVQlQ=").decode("utf-8"),
 )
